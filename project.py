bb0ed2363caf7e8e033285097b8fdfaeebd0c21b
KamTom <kamiltomala04@gmail.com>
2025-03-18 00:24:13 +0100

    Created project.py file and first commit

---
+++ project.py
@@ -0,0 +1,9 @@
+#%%
+
+import cv2  # Importing OpenCV library
+import sys  # Importing sys module for system-specific parameters and functions
+import numpy as np  # Importing numpy library for numerical computations
+#from matplotlib import pyplot
+
+
+#%%
