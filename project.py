001929b40c3bbd8e05e24d097498eba20ec4ea30
KamTom <kamiltomala04@gmail.com>
2025-03-25 01:49:57 +0100

    working version of program

--- project.py
+++
@@ -1,9 +0,0 @@
-#%%
-
-import cv2  # Importing OpenCV library
-import sys  # Importing sys module for system-specific parameters and functions
-import numpy as np  # Importing numpy library for numerical computations
-#from matplotlib import pyplot
-
-
-#%%
